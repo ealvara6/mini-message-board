9cff354f047a6d80799d4b8475a1d188915b59ef
Eduardo Alvarado <ealvara73@gmail.com>
2024-07-30 17:13:16 -0500

    Add color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./views/*.{html,js,ejs}"],
   theme: {
-    extend: {},
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'primary-dark': '#222831',
+        'primary-light': '#393E46',
+        'secondary-dark': '#00ADB5',
+        'secondary-light': '#EEEEEE',
+      },
   },
   plugins: [],
 }
